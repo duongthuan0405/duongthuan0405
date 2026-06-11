8b3f3d8698f3a6a090a63bca9d76b8e3a0742158
duongthuan0405 <duongthuan0405@gmail.com>
2026-06-11 21:35:05 +0700

    TEST: Disconnect Vercel - GitHub

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -2,5 +2,5 @@ import clientEnv from "@/env/client";
 import serverEnv from "@/env/server";
 
 export default async function Home() {
-  return <div>Duong Quoc Thuan - 23521548</div>;
+  return <div>Duong Quoc Thuan - 23521548 - UIT</div>;
 }
